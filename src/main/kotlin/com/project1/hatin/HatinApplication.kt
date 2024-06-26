e8418ac228bf495d5c2f6f40e3e8c8ce57bc0d36
stdiodh <playlistdh@gmail.com>
2024-06-26 16:30:51 +0900

    hatin

---
+++ src/main/kotlin/com/project1/hatin/HatinApplication.kt
@@ -0,0 +1,11 @@
+package com.project1.hatin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HatinApplication
+
+fun main(args: Array<String>) {
+	runApplication<HatinApplication>(*args)
+}
---
+++ src/test/kotlin/com/project1/hatin/HatinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.project1.hatin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HatinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
